df3e1817cd19729cb17cb728b4c03a64b81ceeff
pbalthazar <balthy55@gmail.com>
2018-09-28 12:21:47 -0400

    https

--- SansSoussi/Global.asax.cs
+++ SansSoussi/Global.asax.cs
@@ -29,6 +29,21 @@ namespace SansSoussi
 
         }
 
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            switch (Request.Url.Scheme)
+            {
+                case "https":
+                    Response.AddHeader("Strict-Transport-Security", "max-age=300");
+                    break;
+                case "http":
+                    var path = "https://" + Request.Url.Host + Request.Url.PathAndQuery;
+                    Response.Status = "301 Moved Permanently";
+                    Response.AddHeader("Location", path);
+                    break;
+            }
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
